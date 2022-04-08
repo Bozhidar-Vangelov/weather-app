abee8124e0a4af5e7e9c77ee98f8b095b8728671
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-04-08 11:54:48 +0300

    Add icon and styles to not found component

--- src/shared/components/NotFound.tsx
+++ src/shared/components/NotFound.tsx
@@ -1,7 +1,16 @@
 import { Empty } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const NotFound = () => {
-  return <Empty />;
+  return (
+    <Empty
+      image={<FontAwesomeIcon icon={solid('map-location-dot')} />}
+      description='Location not found.
+    Search must be in the form of "City", "City, State" or "City, Country".'
+      className={'not-found'}
+    />
+  );
 };
 
 export default NotFound;
