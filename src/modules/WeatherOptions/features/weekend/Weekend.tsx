228e6c4ca0323af6380232cfe3237d515097000a
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-04-13 13:06:32 +0300

    Show weekend data

--- src/modules/WeatherOptions/features/weekend/Weekend.tsx
+++ src/modules/WeatherOptions/features/weekend/Weekend.tsx
@@ -1,5 +1,36 @@
+import { Space } from 'antd';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { currentWeatherSelector } from '../../../CurrentWeather/currentWeatherSlice';
+import { fetchWeekendForecast, weekendForecastSelector } from './weekendSlice';
+
 const Weekend = () => {
-  return <div>Weekend</div>;
+  const dispatch = useDispatch();
+  const { weekendForecast, hasFetched } = useSelector(weekendForecastSelector);
+  const { currentWeatherInfo } = useSelector(currentWeatherSelector);
+  const { lat, lon } = currentWeatherInfo.coord;
+
+  useEffect(() => {
+    dispatch(fetchWeekendForecast(lat, lon));
+  }, [dispatch, lat, lon]);
+
+  if (!hasFetched) {
+    return <div>LOADING</div>;
+  }
+
+  return (
+    <Space>
+      {weekendForecast.map((day) => (
+        <div key={day.dt}>
+          <div>Day: {day.dt}</div>
+          <div>Min Temp: {day.temp.min}</div>
+          <div>Max Temp: {day.temp.max}</div>
+          <div>Main Description: {day.weather[0].main}</div>
+          <div>Wind Speed: {day.wind_speed} M/S</div>
+        </div>
+      ))}
+    </Space>
+  );
 };
 
 export default Weekend;
