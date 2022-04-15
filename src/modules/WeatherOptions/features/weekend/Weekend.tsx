275e53ce5cded2c3241fb3bebfd2d063c7a5cba2
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-04-15 11:12:29 +0300

    Add weekend forecast style

--- src/modules/WeatherOptions/features/weekend/Weekend.tsx
+++ src/modules/WeatherOptions/features/weekend/Weekend.tsx
@@ -1,4 +1,6 @@
-import { Space } from 'antd';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Card, Space } from 'antd';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentWeatherSelector } from '../../../CurrentWeather/currentWeatherSlice';
@@ -27,24 +29,44 @@ const Weekend = () => {
   }
 
   return (
-    <Space>
+    <Space className='forecast-container weekend-container'>
       {weekendForecast.map((day) => (
-        <div key={day.dt}>
-          <div>Day: {day.dt}</div>
-          <div>Min Temp: {day.temp.min}</div>
-          <div>Max Temp: {day.temp.max}</div>
-          <div>Main Description: {day.weather[0].main}</div>
-          <div>Wind Speed: {day.wind_speed} m/s</div>
-          <div>Pop: {day.pop} %</div>
-          <div>Clouds: {day.clouds} %</div>
-          <div>
-            Icon:
+        <Card key={day.dt} className='forecast-card'>
+          <Card.Meta title={day.dt} />
+          <Space className='forecast-card-description'>
             <img
               src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
               alt='Icon'
             />
-          </div>
-        </div>
+            <Space direction='vertical'>
+              <Space>{day.weather[0].main}</Space>
+              <Space>
+                <Space align='start'>
+                  <FontAwesomeIcon icon={solid('temperature-arrow-down')} />
+                  {`${day.temp.min}°C`}
+                </Space>
+                <Space>
+                  <FontAwesomeIcon icon={solid('temperature-arrow-up')} />
+                  {`${day.temp.max}°C`}
+                </Space>
+              </Space>
+            </Space>
+          </Space>
+          <Space className='forecast-card-details-container'>
+            <Space>
+              <FontAwesomeIcon icon={solid('cloud')} />
+              {`${day.clouds}%`}
+            </Space>
+            <Space>
+              <FontAwesomeIcon icon={solid('cloud-rain')} />
+              {`${day.pop}%`}
+            </Space>
+            <Space>
+              <FontAwesomeIcon icon={solid('wind')} />
+              {`${day.wind_speed} m/s`}
+            </Space>
+          </Space>
+        </Card>
       ))}
     </Space>
   );
